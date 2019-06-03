e0c5ba36c0551be2b0031d4faa33bc0c1832426d
Amy Westcott <amy.westcott@yahoo.com>
2019-06-03 15:10:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,10 @@
-
+require 'pry'
 def badge_maker(name)
   string = "Hello, my name is #{name}."
 end
 def batch_badge_creator(array)
   array.each do |badge_array|
+  binding.pry
   badge_array.push(badge_maker(name))
   end
   badge_array
