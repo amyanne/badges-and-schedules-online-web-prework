5b90e93a1be883fe3fb41fc792e256d96d679dd6
Amy Westcott <amy.westcott@yahoo.com>
2019-06-03 15:42:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def badge_maker(name)
-  string = "Hello, my name is #{name}."
+  string = "Hello, my name is #{name}\n."
 end
 def batch_badge_creator(array)
   badge_array = []
@@ -13,7 +13,7 @@ def assign_rooms(array)
   room_list = []
   array.each_with_index do |name, index|
     index += 1 
-    room_list.push("Hello, #{name}! You'll be assigned to room #{index}!")
+    room_list.push("Hello, #{name}! You'll be assigned to room #{index}!\n")
   end
  room_list 
 end
@@ -21,9 +21,9 @@ def printer(array)
   badge_array = batch_badge_creator(array)
   room_list = assign_rooms(array)
   badge_array.each do |output|
-    print output
+    print output.chomp
   end
   room_list.each do |output|
-    print output
+    print output.chomp
   end
 end
